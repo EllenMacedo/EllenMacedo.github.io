8a22daed08c96ff94bd3b7c7a445886e9f75aeb8
Ellen Cristhyna Ribeiro Macedo <112514908+EllenMacedo@users.noreply.github.com>
2024-10-23 19:44:50 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Simples alerta ao enviar formulário
+document.getElementById('formContato').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Obrigado por entrar em contato!');
+});
